22718084fd25f8300e31b3f0c4ce24660459ecdc
Tyson2366 <165050812+Tyson2366@users.noreply.github.com>
2025-04-18 13:28:56 +0800

    Add files via upload

--- CityManager.h
+++ CityManager.h
@@ -1,15 +1,1 @@
-
-#import <Foundation/Foundation.h>
-
-@interface CityManager : NSObject
-
-@property (nonatomic, strong) NSDictionary *cityCodeMap;
-
-+ (instancetype)sharedInstance;
-- (NSString *)getCityNameWithCode:(NSString *)code;
-- (NSString *)getProvinceNameWithCode:(NSString *)code;
-- (NSString *)getDistrictNameWithCode:(NSString *)code;
-- (NSString *)getStreetNameWithCode:(NSString *)code;
-- (void)loadCityData;
-
-@end
+// CityManager.h placeholder - content lost after reset
